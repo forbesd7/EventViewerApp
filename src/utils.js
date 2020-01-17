13cce4a238b6f07408ea3d11cab5488db59e9628
Derek <derek.forbes77@gmail.com>
2020-01-17 15:55:35 +0900

    signing in now adds a user to the database

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,28 @@
 import { database, provider, auth } from "./firebase";
 
 const signIn = () => {
-  auth.signInWithPopup(provider).then(result => {
-    database
-      .collection("users")
-      .add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-      })
-      .then(docRef => {
-        console.log("Document written with ID: ", docRef.id);
-        return docRef.id;
+  auth
+    .signInWithPopup(provider)
+    .then(result => {
+      database.collection("users").add({
+        name: result.user.displayName,
+        email: result.user.email
       });
-    console.log(result.user);
+      console.log("user added");
+    })
+    //TODO: put in an error page here
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+const signOut = () => {
+  auth.signOut().then(result => {
+    this.setState({
+      user: null
+    });
+    console.log("signed out");
   });
 };
 
-export { signIn };
+export { signIn, signOut };
